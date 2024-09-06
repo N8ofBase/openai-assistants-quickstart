a15d6fe52b0f51e9334f19d3dd74bb62b8bebd0c
Nate <n8gr8@prismcortex.net>
2024-09-05 19:24:01 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,40 @@ import styles from "./page.module.css";
 
 const Home = () => {
   const categories = {
-    "Basic chat": "basic-chat",
-    "Function calling": "function-calling",
-    "File search": "file-search",
-    All: "all",
+    
+    "Start Chat": "function-calling",
+    
   };
 
   return (
     <main className={styles.main}>
-      <div className={styles.title}>
-        Explore sample apps built with Assistants API
+      <div className={styles.pchead}>        
+      <a href="https://prismcortex.net" target="_blank" rel="noopener noreferrer">
+        <img src="https://s3.amazonaws.com/prismcortex.net/logos/PCLogoSupportblk.png" alt="Prism Cortex Logo" />
+      </a>
+    </div>
+      <div className={styles.title}>      
+      <h3>UrantiaBot</h3>
+      <p>a helpful tool for knowledge and information in<br></br>
+      </p>The Urantia Book
       </div>
+      
       <div className={styles.container}>
         {Object.entries(categories).map(([name, url]) => (
           <a key={name} className={styles.category} href={`/examples/${url}`}>
             {name}
           </a>
         ))}
+      </div>
+
+      <div className={styles.disclaim}>
+        <p>The information provided by this chatbot, powered by OpenAI, is for general informational purposes only. While we strive to offer accurate and up-to-date information, please be aware that the responses generated may not always reflect the most current news, developments, or expert opinions.
+
+This chatbot does not constitute professional advice, and it should not be relied upon as a substitute for specific advice or information from qualified professionals in relevant fields. Users are encouraged to conduct their own research and consult appropriate experts before making decisions based on the information provided here.
+
+Furthermore, the chatbot's responses are generated using artificial intelligence and may include inaccuracies or errors. By using this chatbot, you acknowledge and accept these limitations.
+
+For any urgent inquiries or support, please reach out to our customer service team directly.</p>
       </div>
     </main>
   );
